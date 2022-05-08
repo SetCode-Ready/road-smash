e87ddcf94f53e3f18bb82424873a7f9a018beeec
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-05-08 13:12:01 -0300

    feat: changed background scrolling to script handle

---
+++ scripts/background.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+export (NodePath) var car
+
+var time = 0.0
+
+func _ready() -> void:
+	if car:
+		car = get_node(car)
+	else:
+		set_process(false)
+
+
+func _process(delta: float) -> void:
+	time += delta
+	material.set_shader_param("time", time)
