8dade3e823ff72fe937ad60f63f00bd4381c72c1
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-05-08 13:11:02 -0300

    feat: configured enemy shot

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,17 +1,36 @@
 extends Node2D
 
-const PRE_LAZER = preload("res://scenes/gun bullet.tscn")
+const PRE_LAZER = preload("res://scenes/enemy_gun_bullet.tscn")
+
+onready var velY = rand_range(50, 120)
 
 func _ready():
-	pass 
-
-func _on_Area2D_area_entered(area):
-	get_node("AnimationPlayer").play()
-	get_parent().get_node("car_explosion_fx").play()
+	$shot_timer.wait_time = 2.0
+	$shot_timer.start()
+	
+	
+func _process(delta: float) -> void:
+	global_position.x = clamp(global_position.x, 29, 132)
+	translate(Vector2(0, velY) * delta)
 	
 
+func _on_Area2D_area_entered(area):
+	#get_node("AnimationPlayer").play()
+	#get_parent (). get_node("car_explosion_fx").play()
+	#queue_free()
+	pass
 
 
+#func _on_AnimationPlayer_animation_finished(anim_name):
+#	queue_free()
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+
+func _on_shot_timer_timeout() -> void:
+	var lazer = PRE_LAZER.instance()
+	get_parent().add_child(lazer)
+	lazer.global_position = global_position
+	get_node("gun_shoot_fx").play()
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport: Viewport) -> void:
 	queue_free()
